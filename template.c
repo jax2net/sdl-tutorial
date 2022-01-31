fbd917ac08756258165195636c93bb694be27cba
jax2net <jacob@jax2.net>
2022-01-30 23:03:01 -0800

    Added template file

---
+++ template.c
@@ -0,0 +1,81 @@
+// template.c
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#else
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#endif
+
+#define SCREEN_WIDTH  640
+#define SCREEN_HEIGHT 480
+const char* WINDOW_TITLE = "template.c";
+
+// globals
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+void init();
+void shutdown();
+
+int main() {
+    init();
+
+    SDL_Event e;
+    int exit = 0;
+    
+    while (!exit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) { exit = 1; }
+        }
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(renderer);
+
+        // render here
+        // render();
+
+        SDL_RenderPresent(renderer);
+    }
+
+    shutdown();
+    SDL_Quit();
+    return 0;
+}
+           
+void init() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Failed to initialize SDL: %s\n", SDL_GetError());
+    }
+
+    if (IMG_Init(IMG_INIT_PNG) < 0) {
+        printf("Failed to initialize image subsystem: %s\n", IMG_GetError());
+    }
+
+    if (TTF_Init() < 0) {
+        printf("Failed to initialize TTF subsystem: %s\n", TTF_GetError());
+    }
+    
+    window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                     SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Failed to create window: %s\n", SDL_GetError());
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Failed to create renderer: %s\n", SDL_GetError());
+    }
+}
+
+void shutdown() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    TTF_Quit();
+    IMG_Quit();
+}
