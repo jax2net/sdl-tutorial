caa032fe3da85365bfd075e6a3264aeb9372927d
jax2net <jacob@jax2.net>
2022-02-04 12:38:22 -0800

    Added texture rendering to template

--- lazyfoo/template.c
+++ lazyfoo/template.c
@@ -16,18 +16,48 @@
 #define SCREEN_HEIGHT 480
 const char* WINDOW_TITLE = "template.c";
 
+typedef struct {
+    SDL_Texture* texture;
+    int w, h;
+} GTexture;
+
 // globals
 SDL_Window* window;
 SDL_Renderer* renderer;
+GTexture current_texture;
 
 void init();
 void shutdown();
+void free_texture();
+
+void render(int x, int y, GTexture texture) {
+    SDL_RenderCopy(renderer, texture.texture, NULL, NULL);
+}
+
+void load_from_file(char* path) {
+    free_texture();
+    SDL_Surface* tmp = IMG_Load(path);
+    if (tmp == NULL) {
+        printf("Cannot load surface from path: %s\n", IMG_GetError());
+    }
+    SDL_Texture* result = SDL_CreateTextureFromSurface(renderer, tmp);
+    if (result == NULL) {
+        printf("Cannot create texture from surface: %s\n", SDL_GetError());
+    }
+    current_texture.texture = result;
+    current_texture.w = tmp->w;
+    current_texture.h = tmp->h;
+    SDL_FreeSurface(tmp);
+}
 
 int main() {
     init();
 
     SDL_Event e;
     int exit = 0;
+
+    // setup texture
+    load_from_file("dice.png");
     
     while (!exit) {
         while (SDL_PollEvent(&e) != 0) {
@@ -37,7 +67,7 @@ int main() {
         SDL_RenderClear(renderer);
 
         // render here
-        // render();
+        render(0, 0, current_texture);
 
         SDL_RenderPresent(renderer);
     }
@@ -78,4 +108,13 @@ void shutdown() {
 
     TTF_Quit();
     IMG_Quit();
+}
+
+void free_texture() {
+    if (current_texture.texture != NULL) {
+        SDL_DestroyTexture(current_texture.texture);
+        current_texture.texture = NULL;
+        current_texture.w = 0;
+        current_texture.h = 0;
+    }
 }
