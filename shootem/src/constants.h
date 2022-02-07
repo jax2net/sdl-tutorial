88ca6deee60c48911b9dbcdb239b199f2d0a493b
jax2net <jacob@jax2.net>
2022-02-06 21:27:10 -0800

    Organization and getting Mike on the screen

--- shootem/src/constants.h
+++ shootem/src/constants.h
@@ -4,6 +4,6 @@
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-const char* WINDOW_TITLE = "Shoot 'Em Game";
+extern const char* WINDOW_TITLE;
 
-#endif
+#endif //CONSTANTS_H
--- shootem/src/main.c
+++ shootem/src/main.c
@@ -1,19 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "game.h"
+#include "common.h"
+#include "structs.h"
+#include "draw.h"
 #include "constants.h"
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
+static App app;
+static Entity player;
 
-int main(int argc, char* argv[]) {
+void init(void);
+void shutdown(void);
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+int main(int argc, char** argv) {
+
+    memset(&app, 0, sizeof(App));
+    memset(&player, 0, sizeof(Entity));
+
+    init();
+    
+    SDL_Event e;
+    int exit = 0;
+
+    SDL_Texture* mike = load_texture_from_file("../assets/mike.jpg", app.renderer);
+
+    while (!exit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) { exit = 1; }
+        }
+
+        SDL_SetRenderDrawColor(app.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(app.renderer);
+
+        //render here
+        blit(mike, 50, 50, app.renderer);
+
+        SDL_RenderPresent(app.renderer);
+        SDL_Delay(16);
+    }
+
+    shutdown();
+
+    SDL_Quit();
+    return 0;
+}
+
+
+void init(void) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         printf("Failed to initialize SDL: %s\n", SDL_GetError());
     }
 
-    if (IMG_Init(IMG_INIT_PNG) < 0) {
+    if (IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG) < 0) {
         printf("Failed to initialize image subsystem: %s\n", IMG_GetError());
     }
 
@@ -21,39 +59,23 @@ int main(int argc, char* argv[]) {
         printf("Failed to initialize TTF subsystem: %s\n", TTF_GetError());
     }
     
-    window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                              SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
+    app.window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                  SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (app.window == NULL) {
         printf("Failed to create window: %s\n", SDL_GetError());
     }
 
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL) {
+    app.renderer = SDL_CreateRenderer(app.window, -1, SDL_RENDERER_ACCELERATED);
+    if (app.renderer == NULL) {
         printf("Failed to create renderer: %s\n", SDL_GetError());
     }
-    
-    SDL_Event e;
-    int exit = 0;
+}
 
-    while (!exit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) { exit = 1; }
-        }
-
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        SDL_RenderClear(renderer);
-
-        //render here
-
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+void shutdown(void) {
+    SDL_DestroyRenderer(app.renderer);
+    SDL_DestroyWindow(app.window);
 
     TTF_Quit();
     IMG_Quit();
     
-    SDL_Quit();
-    return 0;
 }
